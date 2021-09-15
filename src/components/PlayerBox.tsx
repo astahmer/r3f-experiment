9001bb6dfe654a6b63cd7c2742947a94c2817f0e
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-09-15 12:52:49 +0200

    feat: useVelocity

--- src/components/PlayerBox.tsx
+++ src/components/PlayerBox.tsx
@@ -1,7 +1,7 @@
 import { useConst, useEventListener } from "@chakra-ui/hooks";
 import { makeArrayOf } from "@pastable/utils";
 import { animated, useSpring } from "@react-spring/three";
-import { Triplet, useBox, usePlane } from "@react-three/cannon";
+import { PublicApi, Triplet, useBox, usePlane } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { DoubleSide, Mesh, MeshBasicMaterial, MeshStandardMaterial, Vector2, Vector3 } from "three";
@@ -18,36 +18,23 @@ export const PlayerBox = () => {
         api.velocity.set(0, 0, 0);
         api.position.set(0, 0, 0);
     });
-    // const pos = useConst<Vector3>((() => initialPos.clone()) as any);
-    // const ref = useRef<Mesh>(null);
-    console.log("render");
 
     const [ref, api] = useBox(() => ({
         mass: 100,
         angularDamping: 1,
         angularVelocity: [1, 1, 1],
         linearDamping: 0.99,
-        // linearFactor: [0.1, 1, 0.1],
         material: { friction: 0 },
         onCollideBegin: (e) => (isGroundedRef.current = true),
         onCollideEnd: (e) => (isGroundedRef.current = false),
     }));
-    const vRef = useRef<Triplet>([0, 0, 0]);
-    const vel = useConst<Vector3>((() => initialPosV.clone()) as any);
-    useEffect(() => {
-        return api.velocity.subscribe((v) => (vRef.current = v));
-    }, []);
+    const vel = useVelocity(api);
 
-    const canDashRef = useRef(true);
     const [isDashing, setDashing] = useState(false);
     const [canDash, setCanDash] = useState(true);
     const isGroundedRef = useRef(false);
 
     useFrame((frame, delta) => {
-        // const offset = 0.02 * playerSpeed;
-        const [vx, vy, vz] = vRef.current;
-        vel.set(vx, vy, vz);
-
         // Set direction
         if (controls.up) vel.setZ(-1 * settings.speed);
         if (controls.down) vel.setZ(1 * settings.speed);
@@ -74,7 +61,6 @@ export const PlayerBox = () => {
             return;
         }
 
-        // if (controls.space) vel.setY(40);
         // Jump
         if (controls.space && isGroundedRef.current) {
             api.applyImpulse([vel.x, settings.jumpForce, vel.z], ref.current.position.toArray());
@@ -83,17 +69,27 @@ export const PlayerBox = () => {
         // Move in direction
         if (controls.anyDir) {
             api.applyImpulse(vel.toArray(), ref.current.position.toArray());
-            // api.velocity.set(...vel.toArray());
-            // console.log(...controls.keys.values());
         }
     });
 
     return (
         <animated.mesh ref={ref} material={isDashing ? dashingMaterial : canDash ? basicMaterial : canDashMaterial}>
             <boxGeometry args={[1, 1, 1]} />
-            {/* <meshStandardMaterial color={isDashing ? "yellow" : "red"} /> */}
         </animated.mesh>
     );
+};
+
+const useVelocity = (api: PublicApi, initialPos: Triplet = [0, 0, 0]) => {
+    const vRef = useRef<Triplet>(initialPos);
+    const vel = useConst<Vector3>((() => new Vector3(...initialPos)) as any);
+
+    useEffect(() => api.velocity.subscribe((v) => (vRef.current = v)), []);
+    useFrame(() => {
+        const [vx, vy, vz] = vRef.current;
+        vel.set(vx, vy, vz);
+    });
+
+    return vel;
 };
 
 const red = new MeshStandardMaterial({ color: "red" });
