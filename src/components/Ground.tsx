6b1e42f376f8ce69d1cf7958e46d6ce485152fee
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-10-06 16:20:32 +0200

    feat(Ground): custom color

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,8 +1,9 @@
-import { Triplet, useBox } from "@react-three/cannon";
+import { useBox } from "@react-three/cannon";
 
 import { CollisionGroup } from "@/functions/store";
+import { CommonObject } from "@/types";
 
-export function Ground({ size = [20, 0.1, 3] }: { size?: Triplet }) {
+export function Ground({ color = "salmon", size = [20, 0.1, 3] }: CommonObject) {
     const [ref] = useBox(() => ({
         type: "Static",
         args: size,
@@ -14,7 +15,7 @@ export function Ground({ size = [20, 0.1, 3] }: { size?: Triplet }) {
     return (
         <mesh ref={ref} name="ground">
             <boxGeometry args={size} />
-            <meshStandardMaterial color="blue" />
+            <meshStandardMaterial color={color} />
         </mesh>
     );
 }
