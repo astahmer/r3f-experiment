2cfeb48870cb78e4aefe648956fbac5f958f2901
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-10-14 20:47:51 +0200

    feat(DumbBox): color change on click + pass meshRef

--- src/components/DumbBox.tsx
+++ src/components/DumbBox.tsx
@@ -1,15 +1,14 @@
-import { DoubleSide } from "three";
+import { useMergeRefs } from "@chakra-ui/hooks";
+import { useRef } from "react";
+import { BoxGeometry, Color, DoubleSide, Mesh, MeshStandardMaterial } from "three";
 
+import { useKey } from "@/functions/useKey";
 import { CommonObject } from "@/types";
 
 import { useObject } from "./Pack";
 
-export function DumbBox({
-    size,
-    position = [5, 1, 1],
-    color = "grey",
-    wireframe = true,
-}: CommonObject & { wireframe?: boolean }) {
+export function DumbBox({ size, position = [5, 1, 1], color = "grey", wireframe = true, meshRef }: CommonObject) {
+    // TODO get default props from pack provider ?
     const [ref, api] = useObject(() => ({
         type: "Static",
         args: size || position,
@@ -19,11 +18,32 @@ export function DumbBox({
         linearDamping: 0.99,
     }));
     // console.log(position);
+    const colorRef = useRef<Color>();
+    const clickedColorRef = useRef(0);
+
+    useKey("n", () => clickedColorRef.current++);
 
     return (
-        <mesh ref={ref} position={position}>
+        <mesh
+            ref={useMergeRefs(ref, meshRef)}
+            position={position}
+            onClick={(e) => {
+                const mesh = e.object as DumbBoxMesh;
+                if (colors.some((color) => mesh.material.color.equals(color))) {
+                    mesh.material.color.set(colorRef.current);
+                    return;
+                }
+
+                colorRef.current = mesh.material.color.clone();
+                mesh.material.color.set(colors[clickedColorRef.current % colors.length]);
+            }}
+        >
             <boxGeometry args={size || position} />
             <meshStandardMaterial color={color} side={DoubleSide} wireframe={wireframe} />
         </mesh>
     );
 }
+
+const colors = [new Color("red"), new Color("black"), new Color("darkblue"), new Color("darkgreen")];
+
+export type DumbBoxMesh = Mesh<BoxGeometry, MeshStandardMaterial>;
