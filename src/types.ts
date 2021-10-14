5c8b5c1d9d7f53fd77dca6ad9208af0faf36a092
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-10-14 20:47:21 +0200

    types: CommonObject add wireframe / meshRef

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 import { Triplet } from "@react-three/cannon";
+import { MutableRefObject, Ref } from "react";
+import { Mesh } from "three";
 
 export interface CommonObject {
     position?: Triplet;
     size?: Triplet;
     rotation?: Triplet;
     color?: string;
+    wireframe?: boolean;
+    meshRef?: MutableRefObject<Mesh> | Ref<Mesh>;
 }
