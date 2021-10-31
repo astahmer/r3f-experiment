a7f471731c461dd3f9108d965d9b28fc0045e91e
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-11-01 00:05:35 +0100

    refactor: DumbBox should be dump + PhysicsBox

--- src/components/DumbBox.tsx
+++ src/components/DumbBox.tsx
@@ -1,13 +1,28 @@
 import { useMergeRefs } from "@chakra-ui/hooks";
-import { useRef } from "react";
-import { BoxGeometry, Color, DoubleSide, Mesh, MeshStandardMaterial } from "three";
+import { BoxGeometry, DoubleSide, Mesh, MeshStandardMaterial } from "three";
 
-import { useKey, useMouseControls } from "@/functions/useKey";
 import { CommonObject } from "@/types";
 
 import { useObject } from "./Pack";
 
 export function DumbBox({
+    size,
+    position = [5, 1, 1],
+    color = "grey",
+    wireframe = true,
+    meshRef,
+    render,
+}: CommonObject) {
+    return (
+        <mesh ref={meshRef} position={position}>
+            <boxGeometry args={size || position} />
+            <meshStandardMaterial color={color} side={DoubleSide} wireframe={wireframe} />
+            {render?.()}
+        </mesh>
+    );
+}
+
+export function PhysicsBox({
     size,
     position = [5, 1, 1],
     color = "grey",
@@ -24,37 +39,14 @@ export function DumbBox({
         angularDamping: 1,
         linearDamping: 0.99,
     }));
-    const colorRef = useRef<Color>();
-    const clickedColorRef = useRef(0);
-
-    const mouse = useMouseControls();
-    const toggle = (e) => {
-        const mesh = e.object as DumbBoxMesh;
-        if (colors.some((color) => mesh.material.color.equals(color))) {
-            mesh.material.color.set(colorRef.current);
-            return;
-        }
-
-        colorRef.current = mesh.material.color.clone();
-        mesh.material.color.set(colors[clickedColorRef.current % colors.length]);
-    };
-
-    useKey("n", () => clickedColorRef.current++);
 
     return (
-        <mesh
-            ref={useMergeRefs(ref, meshRef)}
-            position={position}
-            onPointerDown={toggle}
-            onPointerOver={(e) => mouse.down && toggle(e)}
-        >
+        <mesh ref={useMergeRefs(ref, meshRef)} position={position}>
             <boxGeometry args={size || position} />
             <meshStandardMaterial color={color} side={DoubleSide} wireframe={wireframe} />
             {render?.()}
         </mesh>
     );
 }
-
-const colors = [new Color("red"), new Color("black"), new Color("darkblue"), new Color("darkgreen")];
 
 export type DumbBoxMesh = Mesh<BoxGeometry, MeshStandardMaterial>;
